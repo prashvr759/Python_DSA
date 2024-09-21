3635de9d1190c78508d272c75d538e80c11ef4eb
prashvr759 <64500249+prashvr759@users.noreply.github.com>
2024-09-21 17:48:54 -0400

    Update Valid_Palindrome.py

--- Two_Pointers/Valid_Palindrome.py
+++ Two_Pointers/Valid_Palindrome.py
@@ -1,1 +1,16 @@
+def valid_palindrome(input_array):
+  first = 0
+  last = len(input_array)-1
 
+  while first < last:
+    if len(input_array)=1:
+      return false
+
+    if input_array[first]==input_array[last]:
+      first = first++
+      last = last--
+    else
+      return false
+
+return true
+    
