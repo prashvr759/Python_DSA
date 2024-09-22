ea17f998e6fb414a8a7ba8ab461e67943ba209df
prashvr759 <64500249+prashvr759@users.noreply.github.com>
2024-09-22 11:31:59 -0400

    Update 3Sum.py

--- Two_Pointers/3Sum.py
+++ Two_Pointers/3Sum.py
@@ -1,12 +1,35 @@
 
-//O(N^2) time 
-def bruteforce_3sum():
+# O(N^2) time 
+def bruteforce_3sum(s):
+
+  for i in range(length(s)-1):
+    for j in range(length(s)):
 
 
 
 
 
 
-//O(N) time and O(N) space complexity
-def three_sum(s):
+# O(N) time and O(N) space complexity
+def three_sum(s,total_sum):
+
+  first = 1
+  second = first + 1
+  last = length(s)-1
+  triplet = []
+  while first < last:
+    triplet = s[first] + s[second] + s[last]
+    if triplet == total_sum:
+      return True
+    elif triplet < first:
+      first = first + 1
+      second = second + 1
+    elif triplet > last:
+      last = last -1
+
+  return True
   
+def main():
+
+
+__last
