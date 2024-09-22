10feba9d5b1ec536c8398a1bee637bf1262bbd5d
prashvr759 <64500249+prashvr759@users.noreply.github.com>
2024-09-22 10:49:31 -0400

    Update Valid_Palindrome.py

--- Two_Pointers/Valid_Palindrome.py
+++ Two_Pointers/Valid_Palindrome.py
@@ -1,3 +1,5 @@
+
+// O(N) Time & O(N) Space Complexity Solution
 def valid_palindrome(input_array):
   first = 0
   last = len(input_array)-1
@@ -10,4 +12,15 @@ def valid_palindrome(input_array):
     last = last-1    
 
   return true
-    
+
+# Driver Code
+def main():
+
+    test_cases = ["RACEACAR", "A", "ABCDEFGFEDCBA",
+                  "ABC", "ABCBA", "ABBA", "RACEACAR"]
+    for i in range(len(test_cases)):        
+        print("The input string is '", test_cases[i], "' and the length of the string is ", len(test_cases[i]), ".", sep='')
+        print("\nIs it a palindrome?.....", is_palindrome(test_cases[i]))
+
+if __name__ == '__main__':
+    main()
